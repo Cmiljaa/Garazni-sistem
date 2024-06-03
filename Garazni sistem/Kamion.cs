722d55af26a756a04a2f54d828223781db8bb755
Cmiljaa <147316652+Cmiljaa@users.noreply.github.com>
2024-06-03 03:39:41 +0200

    Added class Kamion

---
+++ Garazni sistem/Kamion.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garazni_sistem
+{
+    internal class Kamion : Vozilo
+    {
+        private double Visina { get; set; }
+        private double Duzina { get; set; }
+
+        public Kamion(int _parking_mesto) : base(_parking_mesto) { Vrsta = "Kamion"; }
+
+        public override void Citaj()
+        {
+            base.Citaj();
+            Console.Write("Unesite visinu:   ");
+            Visina = double.Parse(Console.ReadLine());
+            if (Visina > 2.5)
+                throw new Exception("Kamion je previsok!");
+            Console.Write("Unesite duzinu:   ");
+            Duzina = double.Parse(Console.ReadLine());
+            if (Duzina > 5.3)
+                throw new Exception("Kamion je predugacak!");
+        }
+
+        public override string ToString()
+        {
+            return $"Vrsta vozila: {Vrsta}, {base.ToString()}";
+        }
+    }
+}
