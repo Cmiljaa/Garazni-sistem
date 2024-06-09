c848dea06797c5c6820db027541b891b5803d252
Cmiljaa <147316652+Cmiljaa@users.noreply.github.com>
2024-06-09 19:28:08 +0200

    Error handling added

--- Garazni sistem/Kamion.cs
+++ Garazni sistem/Kamion.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading.Tasks;  
 
 namespace Garazni_sistem
 {
@@ -15,14 +15,30 @@ namespace Garazni_sistem
         public override void Citaj()
         {
             base.Citaj();
-            Console.Write("Unesite visinu: ");
-            Visina = double.Parse(Console.ReadLine());
-            if (Visina > 2.5)
-                throw new Exception("Kamion je previsok!");
-            Console.Write("Unesite duzinu: ");
-            Duzina = double.Parse(Console.ReadLine());
-            if (Duzina > 5.3)
-                throw new Exception("Kamion je predugacak!");
+            try
+            {
+                Console.Write("Unesite visinu: ");
+                double visina = double.Parse(Console.ReadLine());
+                if (visina > 2.5)
+                {
+                    throw new Exception("Kamion je previsok!");
+                }
+
+                Console.Write("Unesite duzinu: ");
+                double duzina = double.Parse(Console.ReadLine());
+                if (duzina > 5.3)
+                {
+                    throw new Exception("Kamion je predugacak!");
+                }
+
+                Visina = visina;
+                Duzina = duzina;
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Neispravan format unosa!");
+                throw;
+            }
         }
 
         public override string ToString()
