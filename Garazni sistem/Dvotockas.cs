bf975307451425d3aeac2baf23fe02a16747e643
Cmiljaa <147316652+Cmiljaa@users.noreply.github.com>
2024-06-09 19:27:38 +0200

    Updated dvotockas

--- Garazni sistem/Dvotockas.cs
+++ Garazni sistem/Dvotockas.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace Garazni_sistem
 {
-    internal class Dvotockas
+    internal class Dvotockas : Vozilo
     {
+        private string Vrsta { get; set; }
+
+        public override void Citaj()
+        {
+            base.Citaj();
+            Console.Write("Unesite vrstu vozila: ");
+            Vrsta = Console.ReadLine();
+        }
+
+        public override string ToString()
+        {
+            return string.Format($"{Vrsta}, Registracija {Registracija}, Vlasnik {Vlasnik}, Telefon {Telefon}, Broj parking mesta {Parking_mesto}");
+        }
     }
 }
